b5dffe63373412b086d39b6ecbf5086d25b0503f
Leonardo Alves <alvesleo2002@gmail.com>
2022-11-21 14:40:17 -0300

    Página de extrato finalizada e responsiva

---
+++ js/extrato.js
@@ -0,0 +1,12 @@
+const valor = document.getElementsByClassName('valor');
+
+const sinal = document.getElementById('pos-neg');
+
+
+if(sinal === '+'){
+    console.log('oi')
+    valor.classList.add('negativo');
+    
+} 
+
+// Continuar dps
