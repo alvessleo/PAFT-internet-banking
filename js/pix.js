ed12edb25f0130565ee3e062e2cadb731dd589df
Leonardo Alves <alvesleo2002@gmail.com>
2022-11-26 17:59:32 -0300

    Adicionado os processos de pagar por pix no javascript

--- js/pix.js
+++ js/pix.js
@@ -9,3 +9,27 @@ step2.style.display = "none";
 // Desabilitando o aviso de pagamento realizado
 const confirm_pix = document.getElementsByClassName("confirm")[0];
 confirm_pix.style.display = "none";
+
+// Tratando o primeiro botao de realizar pagamento pix
+const pag_pix = document.getElementById("pag-pix");
+
+pag_pix.onclick = () => {
+    step1.style.display = "flex";
+}
+
+// Quando o usuario clicar em prosseguir
+const next_step = document.getElementById("next-step");
+
+next_step.onclick = () => {
+    step1.style.display = "none";
+    step2.style.display = "flex";
+    document.querySelector(".info-pix").style.display = "none";
+}
+
+// Quando o usuario clicar em pagar
+const pagar_pix = document.getElementById("pagar-pix");
+
+pagar_pix.onclick = () => {
+    step2.style.display = "none";
+    confirm_pix.style.display = "flex";
+}
