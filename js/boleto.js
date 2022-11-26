1e62d03e86e0a4d53bba42add3caa33ab983084c
Leonardo Alves <alvesleo2002@gmail.com>
2022-11-26 17:43:31 -0300

    Processo para pagar um boleto adicionado por javascript

--- js/boleto.js
+++ js/boleto.js
@@ -9,3 +9,27 @@ step2.style.display = "none";
 // Desabilitando o aviso de pagamento realizado
 const confirm_bol = document.getElementsByClassName("confirm")[0];
 confirm_bol.style.display = "none";
+
+// Tratando o primeiro botao de digitar codigo de barras
+const cod_boleto = document.getElementById("codigo_barras");
+
+cod_boleto.onclick = () => {
+    step2.style.display = "flex";
+}
+
+// Tratanto o botao de pagar
+const pagar = document.getElementById("pagar");
+
+pagar.onclick = () => {
+    step1.style.display = "none"
+    step2.style.display = "none"
+    document.querySelector(".info-boleto").style.display = "none";
+    confirm_bol.style.display = "block"
+}
+
+// Quando o usuario decidir realizar outro pagamento
+const another_payment = document.getElementById("another-payment");
+
+another_payment.onclick = () => {
+    window.location.reload();
+}
