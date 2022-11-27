672a9e3a08f219efd3b97359d9df28710d1d991b
Leonardo Alves <alvesleo2002@gmail.com>
2022-11-27 17:34:07 -0300

    Atualizando os comentarios do empresimo.js

--- js/emprestimo.js
+++ js/emprestimo.js
@@ -41,8 +41,9 @@ new_emprestimo.onclick = () => {
 // Quando o usuario prosseguir o novo emprestimo para selecionar o dia
 const prosseguir_btn = document.getElementById("next2");
 
-prosseguir_btn.disabled = true; // Mudar dps
+prosseguir_btn.disabled = true; // Desabilitando o botão por padrão ate que ele digite um valor de 3 digitos
 
+// Quando o usuario clicar em prosseguir vai liberar o proximo passo
 prosseguir_btn.onclick = () => {
     prosseguir_btn.style.display = "none";
     step2.style.display = "flex";
@@ -51,6 +52,7 @@ prosseguir_btn.onclick = () => {
 // Quando o usuario clicar em realizar emprestimo
 const finish_btn = document.getElementsByClassName("finish")[0];
 
+// Quando o usuario clicar em realizar empresimo tudo some e uma mensagem de sucesso e exibida
 finish_btn.onclick = () => {
     document.querySelector(".info-emprestimo").style.display = "none";
     step1.style.display = "none";
@@ -76,6 +78,7 @@ selectParcelas.addEventListener('input', function(e){
     document.getElementById("per-month").innerText = media(full_value.value,selectParcelas.value).toFixed(2);
 })
 
+// Funcao que usei para liberar o botao de prosseguir com o emprestimo
 function verifica_quantidade(){
     if(full_value.value.length < 3) {
         prosseguir_btn.disabled = true;
