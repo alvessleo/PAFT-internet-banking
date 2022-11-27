99f0d28ad23940678483b6b30e7cfccc5cd41317
Leonardo Alves <alvesleo2002@gmail.com>
2022-11-27 18:22:17 -0300

    Pagina de boleto quase finalizada

--- js/boleto.js
+++ js/boleto.js
@@ -10,6 +10,10 @@ step2.style.display = "none";
 const confirm_bol = document.getElementsByClassName("confirm")[0];
 confirm_bol.style.display = "none";
 
+// Desabilitando o botao de escanear codigo de barras
+const escanear_btn = document.getElementById("escanear");
+escanear_btn.disabled = true;
+
 // Tratando o primeiro botao de digitar codigo de barras
 const cod_boleto = document.getElementById("codigo_barras");
 
@@ -17,8 +21,26 @@ cod_boleto.onclick = () => {
     step1.style.display = "flex";
 }
 
+// Liberando o botao de prosseguir apenas se o usuario colocar o numero do boleto inteiro
+const input_boleto = document.getElementById("codigo-boleto");
+
+input_boleto.addEventListener('input', function (e) {
+    verifica_codigo_boleto();
+});
+
+function verifica_codigo_boleto(){
+    if(input_boleto.value.length < 35) {
+        prosseguir_btn.disabled = true;
+    } else if (input_boleto.value.length >= 35){
+        prosseguir_btn.disabled = false;
+    }
+}
+
+
 // Tratando o botao de prosseguir 
 const prosseguir_btn = document.getElementById("prosseguir-btn");
+
+prosseguir_btn.disabled = true;
 
 prosseguir_btn.onclick = () => {
     document.querySelector(".info-boleto").style.display = "none";
