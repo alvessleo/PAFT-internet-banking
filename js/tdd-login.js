2701b8c9a12ca4b7ae9e0bda6142b819dce09f29
Leonardo Alves <90728273+alvessleo@users.noreply.github.com>
2022-11-29 20:17:30 -0300

    tdd completed

--- js/tdd-login.js
+++ js/tdd-login.js
@@ -24,81 +24,28 @@ validateTest('Foi um sucesso!',() =>{
 
 
 function validate_CPF(cpf, cnpj, senha){
-    if(cpf.value.length < 11 !== "CPF inválido" && senha.value.length < 8 && cnpj.value.length < 14){
-        return
-    }
+    return (cpf.length === 14 && senha.length >= 8 && cnpj.length === 18)
     
 }
 
-// function validateDate(date){
-//     return (!isNaN(new Date(date)) && new Date(date) !== "Invalid Date");
-// }
+console.log(validate_CPF("093.984.839-20", "56.454.353/5353-45", "12345678"));
 
-
-validateTest('Validando login', () => {
-    assert(validate_CPF("093.984.839-20", "56.454.353/5353-45", "12345678910"))
+validateTest('Validando login automatico', () => {
+    assert(validate_CPF("093.984.839-20", "56.454.353/5353-45", "12345678"))
 })
 
-// validateTest('Validando datas',() => {
-//     assert(validateDate("51/01/2022"));
-// });
 
 
 
-document.addEventListener('submit',function(event){
-    event.preventDefault();
-    var elements = document.querySelector('#login');
-    var cpf = elements['cpf'].value;
-    var cnpj = elements['cnpj'].value;
-    var senha = elements['senha'].value;
-
-    let input_cpf = document.getElementById("main-input");
-    input_cpf = cpf;
-    let input_senha = document.getElementById("password-input");
-    input_senha = senha;
-    let input_cnpj = document.getElementById("second-input");
-    input_cnpj = cnpj;
-
-    elements['cpf'].value = '';
-    elements['cnpj'].value = '';
-    elements['senha'].value = '';
-    // elements.submit();
-});
-
-// document.addEventListener('submit',function(event){
-//     event.preventDefault();
-//     var elements = document.querySelector('#tweetForm');
-//     var tweet = elements['tweet'].value;
-
-//     var tweetList = document.querySelector('#tweets');
-//     tweetList.innerHTML += `<li>${tweet}</li>`
-
-//     elements['tweet'].value = '';
-// });
-
-// validateTest('Log In', function(){
-//     var elements = document.querySelector('#login');
-//     elements['cpf'].value = '093.984.839-20';
-//     elements['cnpj'].value = '56.454.353/5353-45';
-//     elements['senha'].value = '12345678';
-
-//     var evt = new Event("submit",{"bubbles":true,"cancelable":true});
-//     elements.dispatchEvent(evt);
-
-//     var login = document.querySelector('#login');
-//     assert(login.innerHTML.includes("<input id='main-input'>093.984.839-20</input>", "<input id='second-input'>56.454.353/5353-45</input>", "<input id='password-input'>12345678</input>"));
-//     login.innerHTML = '';
-// });
+const cpf_input = document.getElementById("main-input");
+const cnpj_input = document.getElementById("second-input");
+const senha_input = document.getElementById("password-input");
 
 
-// validateTest('add new tweet',function(){
-//     var elements = document.querySelector('#tweetForm');
-//     elements['tweet'].value = 'test tweet';
+const checkbox = document.getElementById("checkbox");
 
-//     var evt = new Event("submit",{"bubbles":true,"cancelable":true});
-//     elements.dispatchEvent(evt);
-
-//     var tweetList = document.querySelector('#tweets');
-//     assert(tweetList.innerHTML.toLowerCase().includes("<li>test tweet</li>"));
-//     tweetList.innerHTML = '';
-// });
+checkbox.addEventListener('input', () => {
+    validateTest('Validando login manual', () => {
+        assert(validate_CPF(cpf_input.value , "56.454.353/5353-45", senha_input.value));
+    })
+})
